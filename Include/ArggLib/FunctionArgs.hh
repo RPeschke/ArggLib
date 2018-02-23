ff1e2c1f60455495013d75ead8e651176bd0c6e9
ArggsComputerEcke <arggscomputerecke@gmail.com>
2018-02-23 22:15:14 +0100

    ne named arguments do works so far now the nest step is to make them combinable

--- Include/ArggLib/FunctionArgs.hh
+++ Include/ArggLib/FunctionArgs.hh
@@ -62,49 +62,107 @@ namespace ArggLib {
 	// 	}
 	// 	named_arg(filename);
 	// 	named_arg(extension);
+	//////////////////////////////////////////////////////////////////////////
 
-
+#define  named_arg(name) \
+	template <typename CURRENT_T, typename RET_T, typename T, bool N> \
+		class  CONCATENATE(named_arg_, name) { \
+		public:\
+			const CURRENT_T* m_curr = nullptr;\
+			CONCATENATE(named_arg_, name)(const CURRENT_T* curr) :m_curr(curr) {}\
+			CONCATENATE(named_arg_, name)() = default;\
+			template <typename T1, typename T2, typename T3>\
+			CONCATENATE(named_arg_, name)(const T1* t, T2* t2, T3 * t3) {\
+				m_curr = t;\
+			}\
+			auto name(T x) { return RET_T(*m_curr, CONCATENATE(named_arg_, name)<CURRENT_T, RET_T, T, true>(x)); }\
+	};\
+	template <typename CURRENT_T, typename RET_T, typename T>\
+	class  CONCATENATE(named_arg_, name) <CURRENT_T, RET_T, T, true > {\
+	public:\
+		template <typename T1, typename T2A, typename T2B, typename T3>\
+		CONCATENATE(named_arg_, name)(const T1& t, const   CONCATENATE(named_arg_, name) <T2A, T2B, T, true >* t2, T3*) {\
+			X = t2->CONCATENATE(get_, name)();\
+		}\
+		template <typename T1, typename T2A, typename T2B, typename T3>\
+		CONCATENATE(named_arg_, name)(const T1& t, CONCATENATE(named_arg_, name) <T2A, T2B, T, true >* t2, T3*) {\
+			X = t2->CONCATENATE(get_, name)();\
+		}\
+		template <typename T1, typename T2, typename T3A, typename T3B>\
+		CONCATENATE(named_arg_, name)(const T1& t, T2*, const   CONCATENATE(named_arg_, name) <T3A, T3B, T, true >* t3) {\
+			X = t3->CONCATENATE(get_, name)(); \
+		}\
+		template <typename T1, typename T2, typename T3A, typename T3B>\
+		CONCATENATE(named_arg_, name)(const T1& t, T2*, CONCATENATE(named_arg_, name) <T3A, T3B, T, true >* t3) {\
+			X = t3->CONCATENATE(get_, name)();\
+		}\
+		template <typename T1, typename T2A, typename T3>\
+		CONCATENATE(named_arg_, name)(const T1& t, T2A* t2, T3* t3) {\
+			X = t2->CONCATENATE(get_, name)();\
+		}\
+		template <typename T1, typename T2>\
+		CONCATENATE(named_arg_, name)(const   CONCATENATE(named_arg_, name) <T1, T2, T, true > & t) {\
+			X = t.CONCATENATE(get_, name)();\
+		}\
+        CONCATENATE(named_arg_, name)(T x) : X(x) {}\
+		auto  CONCATENATE(get_, name) ()const { return X; }\
+	private:\
+		T X;\
+	}
+	named_arg(numberA);
+	named_arg(numberB);
+	//////////////////////////////////////////////////////////////////////////
 
 	template <typename CURRENT_T, typename RET_T, typename T, bool N> \
 		class named_arg_date {
 		public:
-			const CURRENT_T* m_curr =nullptr;
-			named_arg_date(const CURRENT_T& curr) :m_curr(&curr) {}
-			named_arg_date(CURRENT_T& curr) :m_curr(&curr) {}
-			template <typename T1>
-			named_arg_date(T1& t) {}
+			const CURRENT_T* m_curr = nullptr;
+			named_arg_date(const CURRENT_T* curr) :m_curr(curr) {}
+
+
 			named_arg_date() = default;
 
-			template <typename T1, typename T2>
-			named_arg_date(const T1* t, T2* t2) {
-				auto x = reinterpret_cast<const CURRENT_T*>(&t);
-			//	X = t2->get_date();
-				//X = x->get_date();
+			template <typename T1, typename T2,typename T3>
+			named_arg_date(const T1* t, T2* t2,T3 * t3) {
 				m_curr = t;
 			}
-			auto date(T x) { return RET_T(*m_curr, named_arg_date<CURRENT_T, RET_T,T,true>( x)); }
+			auto date(T x) { return RET_T(*m_curr, named_arg_date<CURRENT_T, RET_T, T, true>(x)); }
 	};
 	template <typename CURRENT_T, typename RET_T, typename T>
 	class named_arg_date <CURRENT_T, RET_T, T, true > {
 	public:
-		template <typename T1, typename T2>
-		named_arg_date(const T1& t,T2* t2)  {
-			auto x = reinterpret_cast<const CURRENT_T*>(&t);
+		template <typename T1, typename T2A,typename T2B, typename T3>
+		named_arg_date(const T1& t, const  named_arg_date <T2A, T2B, T, true >* t2, T3*) {
+			X = t2->get_date();
+
+		}
+		template <typename T1, typename T2A, typename T2B, typename T3>
+		named_arg_date(const T1& t,   named_arg_date <T2A, T2B, T, true >* t2, T3*) {
+			X = t2->get_date();
+
+		}
+
+		template <typename T1, typename T2, typename T3A, typename T3B>
+		named_arg_date(const T1& t, T2*, const  named_arg_date <T3A, T3B, T, true >* t3) {
+			X = t3->get_date();
+
+		}
+		template <typename T1, typename T2, typename T3A, typename T3B>
+		named_arg_date(const T1& t, T2*,   named_arg_date <T3A, T3B, T, true >* t3) {
+			X = t3->get_date();
+
+		}
+		template <typename T1, typename T2A, typename T3>
+		named_arg_date(const T1& t,T2A* t2, T3* t3) {
 			X = t2->get_date();
 			
 		}
 		template <typename T1, typename T2>
-		named_arg_date(const  named_arg_date <T1, T2, T, true > & t){
-			X = t.get_date();
+		named_arg_date(const  named_arg_date <T1, T2, T, true > & t) {
+			//X = t.get_date();
 		}
-// 		template <typename T1, typename T2>
-// 		named_arg_date(const  named_arg_date <T1, T2, T, false > & t) {
-// 	//		X = t.get_date();
-// 			//auto x = reinterpret_cast<const CURRENT_T*>(&t);
-// 			//X = x->get_date();
-// 		}
 
-		named_arg_date() = default;
+
 		named_arg_date(T x) : X(x) {}
 		auto get_date()const { return X; }
 	private:
@@ -115,12 +173,10 @@ namespace ArggLib {
 		class named_arg_time {
 		public:
 			const CURRENT_T* m_curr = nullptr;
-			named_arg_time(const CURRENT_T& curr) :m_curr(&curr) {}
-			named_arg_time( CURRENT_T& curr) :m_curr(&curr) {}
-			template <typename T1, typename T2>
-			named_arg_time( T1* t,T2* t2) {
-			
-				auto x = reinterpret_cast<const CURRENT_T*>(&t);
+			named_arg_time(const CURRENT_T* curr) :m_curr(curr) {}
+
+			template < typename T2, typename T3>
+			named_arg_time(const CURRENT_T* t, T2* t2, T3* t3) {
 				m_curr = t;
 			}
 			named_arg_time() = default;
@@ -129,31 +185,41 @@ namespace ArggLib {
 	template <typename CURRENT_T, typename RET_T, typename T>
 	class named_arg_time <CURRENT_T, RET_T, T, true > {
 	public:
-	//	CURRENT_T& m_curr;
-	//	named_arg_time(CURRENT_T& curr) :m_curr(curr) {}
-		template <typename T1>
-		named_arg_time(T1 t) {}
+		//	CURRENT_T& m_curr;
+		//	named_arg_time(CURRENT_T& curr) :m_curr(curr) {}
+
 		named_arg_time() = default;
 		named_arg_time(T& x) : X(x) {}
 		named_arg_time(const T& x) : X(x) {}
 		template <typename T1, typename T2>
 		named_arg_time(const  named_arg_time <T1, T2, T, true > & t) {
 			X = t.get_time();
-			//auto x = reinterpret_cast<const CURRENT_T*>(&t);
-			//X = x->get_date();
 		}
-		template <typename T1, typename T2>
-		named_arg_time(const  named_arg_time <T1, T2, T, false > & t) {
-		//	X = t.get_time();
-			//auto x = reinterpret_cast<const CURRENT_T*>(&t);
-			//X = x->get_date();
-		}
-		template <typename T1, typename T2>
-		named_arg_time(T1* t, T2* t2) {
 
-			auto x = reinterpret_cast<const CURRENT_T*>(&t);
-			//m_curr = t;
+
+		template <typename T1, typename T2A, typename T2B , typename  T3>
+		named_arg_time(T1*, const  named_arg_time <T2A, T2B, T, true > * t, T3*) {
+		//	X = t.get_time();
+		}
+		template <typename T1, typename T2A, typename T2B, typename  T3>
+		named_arg_time(T1*,   named_arg_time <T2A, T2B, T, true > * t, T3*) {
+				X = t.get_time();
+		}
+
+		template <typename T1, typename T2, typename  T3A, typename T3B>
+		named_arg_time(T1*, T2*, const  named_arg_time <T3A, T3B, T, true > * t) {
+				X = t->get_time();
+		}
+
+		template <typename T1, typename T2, typename  T3A, typename T3B>
+		named_arg_time(T1*, T2*,   named_arg_time <T3A, T3B, T, true > * t) {
+				X = t->get_time();
+		}
+
+		template <typename T1, typename T2, typename T3>
+		named_arg_time(T1* t, T2* t2, T3 * t3) {
 			X = t2->get_time();
+			
 		}
 		auto get_time() const { return X; }
 	private:
@@ -223,23 +289,56 @@ namespace ArggLib {
 		T1 A;
 	};
 
-	template <bool N1 = false, bool N2 = false>
-	class test3 :public named_arg_date<test3<N1, N2>,test3<true, N2>, int, N1>, public named_arg_time<test3<N1, N2>, test3<N1, true>, int, N2> {
+
+
+	template <bool N1 = false, bool N2 = false, bool N3 = false>
+	class test3 :
+		public named_arg_numberB<
+						test3<N1, N2, N3>, 
+						test3<true, N2, N3>, 
+						int, N1>,
+
+		public named_arg_numberA<
+						test3<N1, N2, N3>, 
+						test3<N1, true, N3>, 
+						int, N2>,
+		
+		public named_arg_date<
+						test3<N1, N2, N3>, 
+						test3<N1, N2, true>, 
+						int, N3> {
 	public:
-		test3():named_arg_date<test3<N1, N2>, test3<true, N2>, int, N1>(*this),  named_arg_time<test3<N1, N2>, test3<N1, true>, int, N2>(*this) {}
-		template <typename T>
-		test3(const T& t3, const  named_arg_time<test3<N1, N2>, test3<N1, true>, int, N2>& t2) :
-			named_arg_date<test3<N1, N2>, test3<true, N2>, int, N1>(this, &t3),
-			named_arg_time<test3<N1, N2>, test3<N1, true>, int, N2>(t2) {}
-		
-		
-		template <typename T,typename T2>
-		test3(const T& t3, const named_arg_date<T2, test3<true, N2>, int, N1>& t2) : 
-			named_arg_time<test3<N1, N2>, test3<N1, true>, int, N2>(this, &t3),
-			named_arg_date<test3<N1, N2>, test3<true, N2>, int, N1>(t2) {}
-		
-		
+		test3() :
+			named_arg_numberB<
+            test3<N1, N2, N3>, 
+            test3<true, N2, N3>, 
+            int, N1>(this),
+
+			named_arg_numberA<
+            test3<N1, N2, N3>, 
+            test3<N1, true, N3>, 
+            int, N2>(this),
+
+
+			named_arg_date<
+            test3<N1, N2, N3>, 
+            test3<N1, N2, true>, 
+            int, N3> (this)
+		{}
+
+
+		template <typename T1, typename T2>
+		test3(const T1& t1, const  T2& t2) :
+			named_arg_numberB<test3<N1, N2, N3>, test3<true, N2, N3>, int, N1>(this, &t1, &t2),
+			named_arg_numberA<test3<N1, N2, N3>, test3<N1, true, N3>, int, N2>(this, &t1, &t2),
+			named_arg_date<   test3<N1, N2, N3>, test3<N1, N2, true>, int, N3>(this, &t1, &t2) {}
+
+
+
+
+
 
 	};
+
 }
 #endif // FunctionArgs_h__
