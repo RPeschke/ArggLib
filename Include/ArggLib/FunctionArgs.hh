71011e4cb91946de03ded89385d412bc11b374b9
ArggsComputerEcke <arggscomputerecke@gmail.com>
2018-02-25 10:21:16 +0100

    stash

--- Include/ArggLib/FunctionArgs.hh
+++ Include/ArggLib/FunctionArgs.hh
@@ -68,6 +68,9 @@ namespace ArggLib {
   template <typename CURRENT_T, typename T, size_t Index, bool N> \
     class CONCATENATE(named_arg_, name) {\
     public:\
+    CONCATENATE(named_arg_, name) (const CONCATENATE(named_arg_, name) <CURRENT_T, T, Index, false >& t):m_curr(t.m_curr) {\
+  std::cout << "SDsadA<false>\n";\
+  }\
       const CURRENT_T* m_curr = nullptr;\
       CONCATENATE(named_arg_, name)(const CURRENT_T* curr) :m_curr(curr) {}\
       CONCATENATE(named_arg_, name)() = default;\
@@ -76,12 +79,15 @@ namespace ArggLib {
         m_curr = t;\
       }\
       auto name(T x) {\
-        return typename CURRENT_T::update<Index>::do_update(*m_curr, CONCATENATE(named_arg_, name)<CURRENT_T, T, Index, true>(x));\
+        return make_test3_update<Index>(*m_curr).do_update(*m_curr, CONCATENATE(named_arg_, name)<CURRENT_T, T, Index, true>(x));\
       }\
   }; \
 	template <typename CURRENT_T,  typename T, size_t Index> \
 	class  CONCATENATE(named_arg_, name)  <CURRENT_T,  T, Index, true >  {\
 	public:\
+    CONCATENATE(named_arg_, name) (const CONCATENATE(named_arg_, name) <CURRENT_T, T, Index, true >& t):X(t.X) {\
+  std::cout << "SDsadA\n";\
+  }\
 		template <typename T1, typename T2A, typename T3>\
   CONCATENATE(named_arg_, name)(const T1& t, const  	CONCATENATE(named_arg_, name) <T2A, T, Index, true >* t2, T3*) {\
     X = t2->CONCATENATE(get_, name)();\
@@ -113,64 +119,7 @@ namespace ArggLib {
 
   //////////////////////////////////////////////////////////////////////////
 
-  template <typename CURRENT_T, typename T, size_t Index, bool N> \
-    class named_arg_date {
-    public:
-      const CURRENT_T* m_curr = nullptr;
-      named_arg_date(const CURRENT_T* curr) :m_curr(curr) {}
-
-
-      named_arg_date() = default;
-
-      template <typename T1, typename T2, typename T3>
-      named_arg_date(const T1* t, T2* t2, T3 * t3) {
-        m_curr = t;
-      }
-      auto date(T x) {
-        return  CURRENT_T::update<Index>::do_update(*m_curr, named_arg_date<CURRENT_T, T, Index, true>(x));
-      }
-  };
-  template <typename CURRENT_T, typename T, size_t Index>
-  class named_arg_date <CURRENT_T, T, Index, true > {
-  public:
-    template <typename T1, typename T2A, typename T3>
-    named_arg_date(const T1& t, const  named_arg_date <T2A, T, Index, true >* t2, T3*) {
-      X = t2->get_date();
-
-    }
-    template <typename T1, typename T2A, typename T3>
-    named_arg_date(const T1& t, named_arg_date <T2A, T, Index, true >* t2, T3*) {
-      X = t2->get_date();
-
-    }
-
-    template <typename T1, typename T2, typename T3A>
-    named_arg_date(const T1& t, T2*, const  named_arg_date <T3A, T, Index, true >* t3) {
-      X = t3->get_date();
-
-    }
-    template <typename T1, typename T2, typename T3A>
-    named_arg_date(const T1& t, T2*, named_arg_date <T3A, T, Index, true >* t3) {
-      X = t3->get_date();
-
-    }
-    template <typename T1, typename T2A, typename T3>
-    named_arg_date(const T1& t, T2A* t2, T3* t3) {
-      X = t2->get_date();
-
-    }
-    template <typename T1>
-    named_arg_date(const  named_arg_date <T1, T, Index, true > & t) {
-      //X = t.get_date();
-    }
-
-
-    named_arg_date(T x) : X(x) {}
-    auto get_date()const { return X; }
-  private:
-    T X;
-  };
-
+/*
   template <typename CURRENT_T, typename T, bool N>
   class named_arg_time {
   public:
@@ -229,7 +178,7 @@ namespace ArggLib {
   private:
     T X;
   };
-
+  */
   template <typename T1, typename T2, bool N1, bool N2>
   class test2 {
   public:
@@ -294,22 +243,13 @@ namespace ArggLib {
   };
 
 
-#define ___ArggLib__test3__Update(Index,...) \
-   template <>\
-  class update<Index> {\
-  public:\
-    template <typename T1, typename T2>\
-    static auto do_update(const T1& t1, const  T2& t2) {\
-    std::cout << "DASD\n";\
-      return test3<__VA_ARGS__>(t1, t2);\
-    }\
-  }
+
   template <typename CURRENT_T, typename T, size_t Index, bool N> \
     class named_arg_numberB;
   template <typename CURRENT_T, typename T, size_t Index, bool N> \
     class named_arg_numberA;
-
-
+  template <typename CURRENT_T, typename T, size_t Index, bool N> \
+    class named_arg_date;
   template <bool N1 = false, bool N2 = false, bool N3 = false>
   class test3 :
     public named_arg_numberB<
@@ -347,23 +287,126 @@ namespace ArggLib {
     test3(const T1& t1, const  T2& t2) :
       named_arg_numberB< test3<N1, N2, N3>, int, 1, N1>(this, &t1, &t2),
       named_arg_numberA< test3<N1, N2, N3>, int, 2, N2>(this, &t1, &t2),
-      named_arg_date<   test3<N1, N2, N3>, int, 3, N3>(this, &t1, &t2) {}
+      named_arg_date<    test3<N1, N2, N3>, int, 3, N3>(this, &t1, &t2) {}
 
 
 
 
-    template <size_t Index>
-    class update {
-    public:
-      template <typename T1, typename T2>
-      static auto do_update(const T1& t1, const  T2& t2);
-    };
-    ___ArggLib__test3__Update(1, true, N2, N3);
-    ___ArggLib__test3__Update(2, N1, true, N3);
-    ___ArggLib__test3__Update(3, N1, N2, true);
 
   };
+  
+  template <size_t Index, bool... N1 >
+  class test3_update;
+
+    
+#define ___ArggLib__test3__Update1(Index,...)\
+  template <bool N1 , bool N2 , bool N3 >\
+  class test3_update<Index,N1,N2,N3> {\
+  public:\
+    template <typename T1 , typename T2>\
+    static auto do_update(const T1& t1, const  T2& t2) {\
+        return test3<__VA_ARGS__>(t1, t2);\
+    }\
+  }
+
+  ___ArggLib__test3__Update1(1, true, N2, N3);
+  ___ArggLib__test3__Update1(2, N1, true, N3);
+  ___ArggLib__test3__Update1(3, N1, N2, true);
+  
+/*
+
+  template <bool N1, bool... NArg>
+  class test3_update<1, N1, NArg...> {
+    public:
+      template <typename T1, typename T2>
+      static auto do_update(const T1& t1, const  T2& t2) {      
+        return test3<true, NArg...>(t1, t2);
+    }
+  };
+  template <bool N1, bool N2, bool... NArg>
+  class test3_update<2, N1, N2, NArg...> {
+  public:
+    template <typename T1, typename T2>
+    static auto do_update(const T1& t1, const  T2& t2) {
+      return test3<N1, true, NArg...>(t1, t2);
+    }
+  };
+  template <bool N1, bool N2, bool N3, bool... NArg>
+  class test3_update<3, N1, N2, N3, NArg...> {
+  public:
+    template <typename T1, typename T2>
+    static auto do_update(const T1& t1, const  T2& t2) {
+      return test3<N1,N2, true, NArg...>(t1, t2);
+    }
+  };
+  */
+  template <size_t Index, bool N1 , bool N2 , bool N3 >
+  auto make_test3_update(const  test3<N1, N2, N3>& t) {
+    return test3_update<Index, N1, N2, N3>();
+  }
   named_arg(numberA);
   named_arg(numberB);
+
+  template <typename CURRENT_T, typename T, size_t Index, bool N> \
+    class named_arg_date {
+    public:
+      const CURRENT_T* m_curr = nullptr;
+      named_arg_date(const CURRENT_T* curr) :m_curr(curr) {}
+
+
+      named_arg_date() = default;
+
+      template <typename T1, typename T2, typename T3>
+      named_arg_date(const T1* t, T2* t2, T3 * t3) {
+        m_curr = t;
+      }
+      auto date(T x) {
+        const CURRENT_T m_currlocal = *m_curr;
+          return make_test3_update<Index>(m_currlocal).do_update(*m_curr, named_arg_date<CURRENT_T, T, Index, true>(x));
+      }
+  };
+  template <typename CURRENT_T, typename T, size_t Index>
+  class named_arg_date <CURRENT_T, T, Index, true > {
+  public:
+    named_arg_date(const named_arg_date <CURRENT_T, T, Index, true >& t):X(t.X) {
+      std::cout << "SDsadA\n";
+    }
+    template <typename T1, typename T2A, typename T3>
+    named_arg_date(const T1& t, const  named_arg_date <T2A, T, Index, true >* t2, T3*) {
+      X = t2->get_date();
+
+    }
+    template <typename T1, typename T2A, typename T3>
+    named_arg_date(const T1& t, named_arg_date <T2A, T, Index, true >* t2, T3*) {
+      X = t2->get_date();
+
+    }
+
+    template <typename T1, typename T2, typename T3A>
+    named_arg_date(const T1& t, T2*, const  named_arg_date <T3A, T, Index, true >* t3) {
+      X = t3->get_date();
+
+    }
+    template <typename T1, typename T2, typename T3A>
+    named_arg_date(const T1& t, T2*, named_arg_date <T3A, T, Index, true >* t3) {
+      X = t3->get_date();
+
+    }
+    template <typename T1, typename T2A, typename T3>
+    named_arg_date(const T1& t, T2A* t2, T3* t3) {
+      X = t2->get_date();
+
+    }
+    template <typename T1>
+    named_arg_date(const  named_arg_date <T1, T, Index, true > & t) {
+      //X = t.get_date();
+    }
+
+
+    named_arg_date(T x) : X(x) {}
+    auto get_date()const { return X; }
+  private:
+    T X;
+  };
 }
 #endif // FunctionArgs_h__
