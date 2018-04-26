bd314a896dbcb607340ab61fadc12bb727cfaa17
arggscomputerecke <arggscomputerecke@gmail.com>
2018-04-26 20:51:02 +0200

    updated function to fill TGrapth.

--- Include/ArggLib/root/Fill_root_objects.hh
+++ Include/ArggLib/root/Fill_root_objects.hh
@@ -2,20 +2,21 @@
 #define ArggLib_Fill_root_objects
 
 #include "TH1D.h"
+#include "TGraph.h"
 #include "ArggLib/type_trates.hh"
 #include <type_traits>
 
 namespace ArggLib {
   template <typename T>
-  class Fill_root_objects_impl {
+  class Fill_root_histogram_impl {
   public:
     T * m_h = nullptr;
     bool do_reset = false;
-    Fill_root_objects_impl& reset_on_start() {
+    Fill_root_histogram_impl& reset_on_start() {
       do_reset = true;
       return *this;
     };
-    Fill_root_objects_impl(T* h) :m_h(h) {}
+    Fill_root_histogram_impl(T* h) :m_h(h) {}
     template <typename NEXT_T, typename... ARGS>
     auto operator()(NEXT_T&& next, ARGS&&... x) ->decltype(next(x...)) {
       m_h->Fill(x...);
@@ -35,19 +36,54 @@ namespace ArggLib {
 
   };
   template <typename T>
-  auto Fill_root_objects(T* t) -> decltype(Fill_root_objects_impl<T>(t)){
-    return  Fill_root_objects_impl<T>(t);
+  auto Fill_histogramm(T* t) -> decltype(Fill_root_histogram_impl<T>(t)){
+    return  Fill_root_histogram_impl<T>(t);
   }
 
 
 
-  template < typename T, typename std::enable_if < std::is_base_of<TObject, T>::value , int > ::type = 0 >
-  auto __helper_to_proc(T* t) -> decltype(Fill_root_objects(t)) {
-    return Fill_root_objects(t);
+  template < typename T, typename std::enable_if < std::is_base_of<TH1, T>::value , int > ::type = 0 >
+  auto __helper_to_proc(T* t) -> decltype(Fill_histogramm(t)) {
+    return Fill_histogramm(t);
   }
-  template < typename T, typename std::enable_if < std::is_base_of<TObject, T>::value, int > ::type = 0 >
-  inline auto __helper_to_proc(T& t) -> decltype(Fill_root_objects(&t)) {
-    return Fill_root_objects(&t);
+  template < typename T, typename std::enable_if < std::is_base_of<TH1, T>::value, int > ::type = 0 >
+  inline auto __helper_to_proc(T& t) -> decltype(Fill_histogramm(&t)) {
+    return Fill_histogramm(&t);
+  }
+
+  template <typename T>
+  class Fill_root_graph_impl{
+  public:
+	  Fill_root_graph_impl(T* graph_) :m_graph(graph_) {}
+	  T* m_graph;
+	  template <typename NEXT_T, typename... ARGS>
+	  auto operator()(NEXT_T&& next, ARGS&&... x) ->decltype(next(x...)) {
+		  
+		  m_graph->SetPoint(m_graph->GetN(), x...);
+	
+		  return next(x...);
+	  }
+
+
+	  auto End() ->decltype(m_graph) {
+
+		  return   m_graph;
+	  }
+
+  };
+
+  template <typename T>
+  auto Fill_graph(T* t) -> decltype(Fill_root_graph_impl<T>(t)) {
+	  return  Fill_root_graph_impl<T>(t);
+  }
+
+  template < typename T, typename std::enable_if < std::is_base_of<TGraph, T>::value, int > ::type = 0 >
+  auto __helper_to_proc(T* t) -> decltype(Fill_graph(t)) {
+	  return Fill_graph(t);
+  }
+  template < typename T, typename std::enable_if < std::is_base_of<TGraph, T>::value, int > ::type = 0 >
+  inline auto __helper_to_proc(T& t) -> decltype(Fill_graph(&t)) {
+	  return Fill_graph(&t);
   }
 
   class Draw_root_objects {
